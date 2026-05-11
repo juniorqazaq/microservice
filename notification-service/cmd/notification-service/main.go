7140339c8fff1bc272bb3ace7dd436a617fcd77f
Sanat-07 <bgenbaevsanat@gmail.com>
2026-05-11 05:16:55 +0500

    feat(notification): wire redis and email sender

--- notification-service/cmd/notification-service/main.go
+++ notification-service/cmd/notification-service/main.go
@@ -7,15 +7,35 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/redis/go-redis/v9"
 	"notification-service/internal/consumer"
+	"notification-service/internal/provider"
 )
 
 func main() {
 	rmqURL := mustEnv("RABBITMQ_URL")
+	redisAddr := mustEnv("REDIS_ADDR")
+	providerMode := os.Getenv("PROVIDER_MODE")
 
 	log.Println("Starting Notification Service...")
 
-	cons, err := consumer.NewRabbitMQConsumer(rmqURL)
+	redisClient := redis.NewClient(&redis.Options{Addr: redisAddr})
+	defer redisClient.Close()
+
+	var emailSender provider.EmailSender
+	if providerMode == "REAL" {
+		emailSender = provider.NewRealProvider(
+			mustEnv("SMTP_HOST"),
+			mustEnv("SMTP_PORT"),
+			mustEnv("SMTP_USER"),
+			mustEnv("SMTP_PASSWORD"),
+			mustEnv("SMTP_FROM"),
+		)
+	} else {
+		emailSender = provider.NewSimulatedProvider()
+	}
+
+	cons, err := consumer.NewRabbitMQConsumer(rmqURL, redisClient, emailSender)
 	if err != nil {
 		log.Fatalf("failed to initialize consumer: %v", err)
 	}
